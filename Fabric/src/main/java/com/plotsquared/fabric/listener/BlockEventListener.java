f2bb6307fa9600999f704cec9546df8f36c7b6f5
RacoonDog <32882447+racoondog@users.noreply.github.com>
2025-08-24 11:59:36 -0400

    fix `BlockEventListener` dispenser handling

--- Fabric/src/main/java/com/plotsquared/fabric/listener/BlockEventListener.java
+++ Fabric/src/main/java/com/plotsquared/fabric/listener/BlockEventListener.java
@@ -1020,7 +1020,7 @@ public class BlockEventListener {
             return InteractionResult.PASS;
         }
         Item type = stackToDispense.getItem();
-        switch (type.toString().toUpperCase().substring(type.toString().indexOf(":"))) {
+        switch (BuiltInRegistries.ITEM.getKey(type).getPath().toUpperCase()) {
             case "SHULKER_BOX", "WHITE_SHULKER_BOX", "ORANGE_SHULKER_BOX", "MAGENTA_SHULKER_BOX", "LIGHT_BLUE_SHULKER_BOX",
                     "YELLOW_SHULKER_BOX", "LIME_SHULKER_BOX", "PINK_SHULKER_BOX", "GRAY_SHULKER_BOX", "LIGHT_GRAY_SHULKER_BOX",
                     "CYAN_SHULKER_BOX", "PURPLE_SHULKER_BOX", "BLUE_SHULKER_BOX", "BROWN_SHULKER_BOX", "GREEN_SHULKER_BOX",
